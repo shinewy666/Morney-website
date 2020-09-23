c31530644e9ef6ce056e81826c0072c00229337b
shine_xyy <1518030666@qq.com>
2020-09-24 04:16:31 +0800

    deploy

---
+++ precache-manifest.9530fe5860863291c2ab68739f572e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5007265d4d8bd65c109c",
+    "url": "/Morney-website/css/app.5bcd2c4e.css"
+  },
+  {
+    "revision": "dc5bb05b964c4e638922d906a0b2c16f",
+    "url": "/Morney-website/index.html"
+  },
+  {
+    "revision": "5007265d4d8bd65c109c",
+    "url": "/Morney-website/js/app.1918393d.js"
+  },
+  {
+    "revision": "1406e6c454ca44838d0c",
+    "url": "/Morney-website/js/chunk-vendors.62fc997f.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website/robots.txt"
+  }
+]);
